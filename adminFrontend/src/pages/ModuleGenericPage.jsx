116a252b54b46f2794c977f6245131be5828a6bc
Suraj Prasad <146601188+SurajGupta7224@users.noreply.github.com>
2025-12-08 22:06:39 +0530

    Delete adminFrontend directory

--- adminFrontend/src/pages/ModuleGenericPage.jsx
+++
@@ -1,3 +0,0 @@
-export default function ModuleGenericPage({ module }) {
-  return <h1>{module.title} Page (No custom UI yet)</h1>;
-}
--- adminFrontend/src/pages/UsersPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function UsersPage() {
-  return (
-    <div className="p-6 ml-72">
-      <h1 className="text-2xl font-bold">Welcome to Users module</h1>
-    </div>
-  );
-}
